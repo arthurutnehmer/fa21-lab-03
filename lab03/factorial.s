9e80e3eb4ea7c73cc51d5aeb1846f5960715e507
Arthur Utnehmer <arthurutnehmer@berkeley.edu>
2021-09-24 15:46:49 -0700

    srd

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,21 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+	#Iterator 
+	addi a1, x0, 1
+    #Saved value
+	addi a2, x0, 1
+    #Our N value +1
+    addi a3, a0, 1
+    
+    #Do while out n value is not equal to iterator
+    loop:
+    	beq a3, a1, exit
+        mul a2, a2, a1
+        addi a1, a1, 1
+        jal x0, loop
+        
+    exit:
+    	add a0, a2, x0
+        jr ra
+
